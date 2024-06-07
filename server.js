3bcc2505e75d386ea7f7b9995d4dbe8f9f76f3c0
mlbboy1124 <43491856+mlbboy1124@users.noreply.github.com>
2024-06-07 21:59:26 +0900

    update DB issue v11

--- server.js
+++ server.js
@@ -3,37 +3,28 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 require('dotenv').config();
 
-
-//라우터 설정
+// 라우터 설정
 const authRoutes = require('./routes/authRoutes');
-
 const realregRoutes = require('./routes/realregRoutes');
 const realregReceiptRoutes = require('./routes/realregReceiptRoutes');
-
 const lawsuitRoutes = require('./routes/lawsuitRoutes');
 const lawsuitReceiptRoutes = require('./routes/lawsuitReceiptRoutes');
-
 const eventsRoutes = require('./routes/eventsRoutes');
 
-
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors()); // 모든 도메인에서 오는 요청을 허용
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//라우터 사용
+// 라우터 사용
 app.use('/api', authRoutes);
-
 app.use('/api', realregRoutes);
 app.use('/api', realregReceiptRoutes);
-
 app.use('/api', lawsuitRoutes);
 app.use('/api', lawsuitReceiptRoutes);
-
 app.use('/api', eventsRoutes);
-
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
