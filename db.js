549d8bf9ecf17a767f7c9ce7c84d562cf922c3cd
mlbboy1124 <43491856+mlbboy1124@users.noreply.github.com>
2024-06-07 20:47:46 +0900

    update DB issue v6

--- db.js
+++ db.js
@@ -1,5 +1,12 @@
 const mysql = require('mysql2/promise');
 
+// 환경 변수 출력
+console.log('DB_HOST:', process.env.DB_HOST);
+console.log('DB_PORT:', process.env.DB_PORT);
+console.log('DB_USERNAME:', process.env.DB_USERNAME);
+console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
+console.log('DB_NAME:', process.env.DB_NAME);
+
 const masterPool = mysql.createPool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
