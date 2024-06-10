0a2aae11f7b8eab0314923cc3c4fe2e23e107e3f
mlbboy1124 <43491856+mlbboy1124@users.noreply.github.com>
2024-06-10 12:49:58 +0900

    update refresh issue v4

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+var path = require('path');
 const history = require('connect-history-api-fallback'); // 추가된 부분
+
 require('dotenv').config();
 
 // 라우터 설정
@@ -18,9 +20,8 @@ const port = process.env.PORT || 3000;
 app.use(cors()); // 모든 도메인에서 오는 요청을 허용
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(history()); //여기
 
-// History API fallback을 사용하도록 설정 (라우터 설정 이전에 사용)
-app.use(history());
 
 // 라우터 사용
 app.use('/api', authRoutes);
@@ -30,13 +31,7 @@ app.use('/api', lawsuitRoutes);
 app.use('/api', lawsuitReceiptRoutes);
 app.use('/api', eventsRoutes);
 
-// 정적 파일 서빙 (build 폴더를 프론트엔드 빌드 출력 디렉토리로 변경)
-app.use(express.static('build'));
-
-// catch-all route to serve index.html (SPA의 엔트리 포인트로 사용)
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
-});
+app.use(express.static(path.join(__dirname, 'public'))); // 반드시 여기 위에 use 쓸 것!
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
