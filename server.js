1bb82da387405539444e1a4fa7b74d08ad3448f0
mlbboy1124 <43491856+mlbboy1124@users.noreply.github.com>
2024-06-10 12:39:35 +0900

    update refresh issue v3

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const history = require('connect-history-api-fallback'); // 추가된 부분
 require('dotenv').config();
 
 // 라우터 설정
@@ -18,6 +19,9 @@ app.use(cors()); // 모든 도메인에서 오는 요청을 허용
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// History API fallback을 사용하도록 설정 (라우터 설정 이전에 사용)
+app.use(history());
+
 // 라우터 사용
 app.use('/api', authRoutes);
 app.use('/api', realregRoutes);
@@ -25,6 +29,14 @@ app.use('/api', realregReceiptRoutes);
 app.use('/api', lawsuitRoutes);
 app.use('/api', lawsuitReceiptRoutes);
 app.use('/api', eventsRoutes);
+
+// 정적 파일 서빙 (build 폴더를 프론트엔드 빌드 출력 디렉토리로 변경)
+app.use(express.static('build'));
+
+// catch-all route to serve index.html (SPA의 엔트리 포인트로 사용)
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+});
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
