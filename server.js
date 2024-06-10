4bdd9171261ec38f18a5fda4622072e315f75f83
mlbboy1124 <43491856+mlbboy1124@users.noreply.github.com>
2024-06-10 12:56:16 +0900

    update refresh issue v5

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-var path = require('path');
-const history = require('connect-history-api-fallback'); // 추가된 부분
-
 require('dotenv').config();
 
 // 라우터 설정
@@ -20,8 +17,6 @@ const port = process.env.PORT || 3000;
 app.use(cors()); // 모든 도메인에서 오는 요청을 허용
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(history()); //여기
-
 
 // 라우터 사용
 app.use('/api', authRoutes);
@@ -30,8 +25,6 @@ app.use('/api', realregReceiptRoutes);
 app.use('/api', lawsuitRoutes);
 app.use('/api', lawsuitReceiptRoutes);
 app.use('/api', eventsRoutes);
-
-app.use(express.static(path.join(__dirname, 'public'))); // 반드시 여기 위에 use 쓸 것!
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
