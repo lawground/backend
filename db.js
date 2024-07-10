fec410fe0367b1b9accf1395c7840020b69bd7e6
mlbboy1124 <43491856+mlbboy1124@users.noreply.github.com>
2024-07-10 11:18:22 +0900

    debug v3

--- db.js
+++ db.js
@@ -1,30 +1,30 @@
 const mysql = require('mysql2/promise');
 
 // 클라우드 접속
-// const masterPool = mysql.createPool({
-//   host: process.env.DB_HOST,
-//   port: process.env.DB_PORT,
-//   user: process.env.DB_USERNAME,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME,
-//   waitForConnections: true,
-//   connectionLimit: 10,
-//   queueLimit: 0,
-//   charset: 'utf8mb4'
-// });
-
-// 로컬 접속
 const masterPool = mysql.createPool({
-  host: '127.0.0.1',
-  port: '3306',
-  user: 'root',
-  password: '1q2w3e4r..',
-  database: 'masterdb',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
   charset: 'utf8mb4'
 });
+
+// 로컬 접속
+// const masterPool = mysql.createPool({
+//   host: '127.0.0.1',
+//   port: '3306',
+//   user: 'root',
+//   password: '1q2w3e4r..',
+//   database: 'masterdb',
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0,
+//   charset: 'utf8mb4'
+// });
 
 masterPool.getConnection()
   .then(connection => {
